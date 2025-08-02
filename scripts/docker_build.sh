1efd740e216aa446f5d89b09bf34f096c7af6b3e
tihaya-anon <chihaya_anon_0908@outlook.com>
2025-08-02 18:01:13 +0800

    feat: install official postgresql helm chart

---
+++ scripts/docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR/..
+docker build -f pkg/app/Dockerfile -t event_repository_app .
+
--- scripts/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR/../src/cmd/
-go mod tidy
-echo "Building app..."
-go build -o ../../build/app
-cd $SCRIPT_DIR
-../build/app
